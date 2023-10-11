360d43f85c5960277567433a59f7c3287692576a
carbingus <exthalia2015@gmail.com>
2023-10-11 15:44:01 -0300

    cambiar contenido articulo - arreglar validacion datos y finalizar - remover script para caja comentarios

--- escripos/escriposCopitos.js
+++ escripos/escriposCopitos.js
@@ -10,26 +10,90 @@ function mostrarContenido(id) {
     }
 }
 
-//script para carrousel
+function cambiarContenido(botonId) {
+  // Get the content element.
+  const elementoContenido = document.querySelector('#contenido');
+
+  // Set the content element's innerHTML property to the content for the button that was clicked.
+  elementoContenido.textContent = recibirContenidoPorBoton(botonId);
+}
+
+function recibirContenidoPorBoton(botonId) {
+  // Get the content for the button that was clicked.
+  // This content can be stored in a JavaScript object, a database, or anywhere else that is convenient for you.
+
+  const contenido = {
+    'cambiarContenido': 'Contenido pal boton "london".',
+    'cambiarContenido1': 'Contenido pal boton Paris .',
+    'cambiarContenido2': 'Contenido pal botonTokyo .',
+    'cambiarContenido3': 'Contenido original aca .',
+  };
+
+  return contenido[botonId];
+}
+
+// TODO: script para carrousel
+
+function carrusito(){
+    let arrayLoco = [];
+    arrayLoco.push(['img/3b1.jpg', 'Gato blin', '720px', '720px']);
+    arrayLoco.push(['img/media wuna.png', 'Quaso', '720px', '720px']);
+    arrayLoco.push(['img/mistakes.png', 'Errores', '720px', '720px']);
+
+    
+}
+
+let primero = document.querySelector("form input");
+primero.focus();
+let reg_correo = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
 
 function validarDatos() {
-    let nombreValid = document.forms["contacting"]["nombre"].value;
-    let emailValid = document.forms["contacting"]["email"].value;
-    let mensajeValid = document.forms["contacting"]["mensaje"].value;
+    let nombreValid = document.querySelector("nombre");
+    let emailValid = document.querySelector("email");
+    let mensajeValid = document.querySelector("mensaje");
+    
+  
+    // $( "<p id='error_div'>Test</p>" ).appendTo( ".inner" ); / append
+    // $( "#error_div" ).remove(); / remover
+    if (nombreValid == "" || nombreValid == null) {
+      //mostrar error en html
+    }
+  
+    if (emailValid != "" && !reg_correo.test(emailValid.value)) {
+    
+      //mostrar error en html
 
-    if (nombreValid == "" || nombreValid == null){
-        alert("Usted debe ingresar su nombre.")
-        return false;
     }
-    if (emailValid == "" || emailValid == null){
-        alert("Usted debe ingresar su correo.")
-        return false;
-    }
-    if (mensajeValid == "" || mensajeValid == null){
-        alert("Usted no ha escrito ningun mensaje para enviar.")
-        return false;
-    } else if (mensajeValid.length<6){
-        alert("Su mensaje no puede ser menor que 6 caracteres.")
+  
+    if (mensajeValid == "" || mensajeValid == null) {
+      
+      //mostrar error en html
+      
+    } else if (mensajeValid.length < 10) {
+      
+      //mostrar error en html
+      
     }
 
-}
+    if (terror){
+      errores.style.display = "initial";
+      return false;
+    } else{
+      let p = document.createElement("p");
+      if (emailValid.value != ""){
+        p.innerHTML =
+        nombreValid.value + "(" + emailValid.value + ")"+ " ha escrito \n" +
+        mensajeValid.value + ".";
+      } else{
+        p.innerHTML =
+        nombreValid.value + " ha escrito; \n" +
+        mensajeValid.value + ".";
+      }
+      mensajes.appendChild(p);
+      document.forms["contacting"].reset();
+      let primero = document.querySelector("form input");
+      primero.focus();
+    }
+    return false;
+  }
+
